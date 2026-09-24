910c7b9e56c8abaf8378dd65bb93daa7bcbc7f4e
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-24 08:18:01 -0500

    feat(iam): crear entidad de usuario, configuracion ef core y seed de administrador

---
+++ AllenStock.API/Migrations/20260924131643_AddIamUserTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AllenStock.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIamUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombre_completo = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    correo = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    contrasena_hash = table.Column<string>(type: "text", nullable: false),
+                    rol = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false, defaultValue: "Cajero"),
+                    activo = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    fecha_creacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "correo", "nombre_completo", "activo", "contrasena_hash", "rol" },
+                values: new object[] { 1, "admin@allentech.com", "Gustavo Alonso Olivares Lao", true, "$2a$11$0uM.3C/1.BfP/K3nF4L33O3yJz2hR6JgO6.M3XlY1iM3J1.3.3.3.", "Administrador" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_correo",
+                table: "Usuarios",
+                column: "correo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
