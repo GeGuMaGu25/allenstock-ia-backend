c48dd55b4160659295054bd603dcf4026c8940c2
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-24 08:30:14 -0500

    feat(iam): implementar validacion bcrypt, emision de tokens jwt y login endpoint

---
+++ AllenStock.API/IAM/Application/DTOs/AuthDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace AllenStock.API.IAM.Application.DTOs;
+
+/// <summary>
+/// DTOs para el inicio de sesión.
+/// Creado por: Gustavo Alonso Olivares Lao
+/// </summary>
+public record SignInRequestDto(string correo, string contrasena);
+
+public record SignInResponseDto(int id, string nombre_completo, string rol, string token);
---
+++ AllenStock.API/IAM/Application/Services/IIamService.cs
@@ -0,0 +1,8 @@
+﻿using AllenStock.API.IAM.Application.DTOs;
+
+namespace AllenStock.API.IAM.Application.Services;
+
+public interface IIamService
+{
+    Task<SignInResponseDto?> SignInAsync(SignInRequestDto request);
+}
