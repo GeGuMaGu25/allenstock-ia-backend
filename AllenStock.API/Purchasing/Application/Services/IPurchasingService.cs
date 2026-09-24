d06be0e76a913f351b62a56b3eeffc9220d59a50
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-24 09:04:31 -0500

    feat(purchasing): implementar dtos, servicio y endpoint get para listado de reclamos

---
+++ AllenStock.API/Purchasing/Application/Services/IPurchasingService.cs
@@ -0,0 +1,8 @@
+﻿using AllenStock.API.Purchasing.Application.DTOs;
+
+namespace AllenStock.API.Purchasing.Application.Services;
+
+public interface IPurchasingService
+{
+    Task<IEnumerable<ClaimResponseDto>> GetActiveClaimsAsync();
+}
