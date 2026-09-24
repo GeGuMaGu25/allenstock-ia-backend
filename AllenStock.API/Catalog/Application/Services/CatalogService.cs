776bf258403ff0a71e468925a6ab3a8894cfe016
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-24 07:27:10 -0500

    feat(catalog): implementar dtos, aplicacion y minimal api de productos con cors

---
+++ AllenStock.API/Catalog/Application/Services/CatalogService.cs
@@ -0,0 +1,34 @@
+﻿using AllenStock.API.Catalog.Application.DTOs;
+using AllenStock.API.Shared.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllenStock.API.Catalog.Application.Services;
+
+/// <summary>
+/// Servicio de aplicación para la lógica del Catálogo.
+/// Creado por: Gustavo Alonso Olivares Lao
+/// </summary>
+public class CatalogService : ICatalogService
+{
+    private readonly AppDbContext _context;
+
+    // Inyección de Dependencias: Pedimos el contexto de la base de datos
+    public CatalogService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<ProductResponseDto>> GetAllProductsAsync()
+    {
+        return await _context.Products
+            .Include(p => p.Category) // Hacemos un JOIN con la tabla Categorías
+            .Select(p => new ProductResponseDto(
+                p.Id,
+                p.Barcode,
+                p.Name,
+                p.Category.Name, // Sacamos el nombre de la categoría relacionada
+                p.SalePrice
+            ))
+            .ToListAsync();
+    }
+}
---
+++ AllenStock.API/Catalog/Application/Services/ICatalogService.cs
@@ -0,0 +1,8 @@
+﻿using AllenStock.API.Catalog.Application.DTOs;
+
+namespace AllenStock.API.Catalog.Application.Services;
+
+public interface ICatalogService
+{
+    Task<IEnumerable<ProductResponseDto>> GetAllProductsAsync();
+}
