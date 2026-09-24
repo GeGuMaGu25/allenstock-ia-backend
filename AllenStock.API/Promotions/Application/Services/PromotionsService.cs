4c76fb2a42ee6b8bad9d4e17ff4f095d8f7c4a44
GeGuMaGu25 <gustavoolivares302@gmail.com>
2026-09-24 10:11:58 -0500

    feat(promotions): agregar dto, servicio y endpoint get para listar promociones activas con join a productos

--- AllenStock.API/Promotions/Application/Services/PromotionsService.cs
+++ AllenStock.API/Promotions/Application/Services/PromotionsService.cs
@@ -1,12 +1,14 @@
 ﻿using AllenStock.API.Promotions.Application.DTOs;
 using AllenStock.API.Promotions.Domain.Entities;
 using AllenStock.API.Shared.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace AllenStock.API.Promotions.Application.Services;
 
 public interface IPromotionsService
 {
     Task<bool> ApplyDiscountAsync(ApplyPromotionRequestDto dto);
+    Task<IEnumerable<PromotionResponseDto>> GetActivePromotionsAsync(); // <- Nueva línea
 }
 
 /// <summary>
@@ -20,6 +22,30 @@ public class PromotionsService : IPromotionsService
     public PromotionsService(AppDbContext context)
     {
         _context = context;
+    }
+    
+    public async Task<IEnumerable<PromotionResponseDto>> GetActivePromotionsAsync()
+    {
+        var activePromotions = await _context.Promotions
+            .Where(p => p.Status == "Activa")
+            .Join(
+                _context.Products, 
+                promocion => promocion.ProductId, 
+                producto => producto.Id, 
+                (promocion, producto) => new PromotionResponseDto(
+                    promocion.Id,
+                    promocion.ProductId,
+                    producto.Name,
+                    promocion.DiscountPercentage,
+                    promocion.Reason,
+                    promocion.Status,
+                    promocion.CreatedAt.ToString("yyyy-MM-dd HH:mm")
+                )
+            )
+            .OrderByDescending(p => p.fecha_creacion)
+            .ToListAsync();
+
+        return activePromotions;
     }
 
     public async Task<bool> ApplyDiscountAsync(ApplyPromotionRequestDto dto)
